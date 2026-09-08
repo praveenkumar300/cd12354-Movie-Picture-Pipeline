b5db6b9ebbeaca92998e0a5a2a825367bbb709c2
Praveen kumar <chpraveen3000@gmail.com>
2026-09-08 21:17:54 +0530

    testing frontend pull request

--- starter/frontend/src/App.js
+++ starter/frontend/src/App.js
@@ -3,6 +3,7 @@ import MovieList from './components/MovieList';
 import MovieDetails from './components/MovieDetails';
 import './App.css';
 
+// Added to check frontend ci 
 export default function App() {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
